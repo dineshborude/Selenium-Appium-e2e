fcfd484d936080c81cadfd7fb693a6d25f80c316
Dinesh Borude <borudedinesh2@gmail.com>
2024-09-23 00:30:44 +0530

    selenium restart

--- src/test/java/tests/web/BaseClass_Web.java
+++ src/test/java/tests/web/BaseClass_Web.java
@@ -1,5 +1,20 @@
 package tests.web;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 public class BaseClass_Web {
+
+    WebDriver webdriver;
+
+    @Test
+    public void launchWebApp() {
+
+        webdriver = new ChromeDriver();
+
+        webdriver.get("https://www.google.com");
+
+
+    }
+
 }
